2fc2329bb8f35304d2e972eba9ee974b3269249d
bill Jiang <475572229@qq.com>
2017-08-29 13:57:16 +0800

    file upload service

--- springcloud-demo/service/file-upload/src/main/java/com/example/fileupload/FileUploadApplication.java
+++ springcloud-demo/service/file-upload/src/main/java/com/example/fileupload/FileUploadApplication.java
@@ -1,16 +1,15 @@
-package com.example.configserver;
+package com.example.fileupload;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
-import org.springframework.cloud.config.server.EnableConfigServer;
 
 @SpringBootApplication
-@EnableConfigServer
 @EnableDiscoveryClient
-public class ConfigserverApplication {
+public class FileUploadApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(ConfigserverApplication.class, args);
+		SpringApplication.run(FileUploadApplication.class, args);
 	}
 }
